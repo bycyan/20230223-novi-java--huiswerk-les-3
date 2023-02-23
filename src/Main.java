32626ac0be20e2d5f8d2d8f1572e81c3f5e2ade4
bycyan <cyan@wervelwindmedia.nl>
2023-02-23 20:10:04 +0100

    tot stap 6

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Hier mag je je code scrijven voor de hoofd-opdracht
+        // Hier mag je je code schrijven voor de hoofd-opdracht
+
+        ArrayList<Integer> numeric = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 0));
+        ArrayList<String> alphabetic = new ArrayList<>(List.of("een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "nul"));
+
+    }
+}
+
+
+//        boolean b = set.add(105);
+//        if (set.add(101)) {
+//            System.out.println("101 toegevoegd");
+//        }
+//        else {
+//            System.out.println("101 niet toegevoegd");
+//        }
+//        System.out.println(set);
 
         /* deze regel mag je weg halen voor de bonus opdracht. Onderstaande code is voor de bonus opdracht.
         HashSet<Integer> secretnumber = randomnumbergenerator();
@@ -14,29 +30,27 @@ public class Main {
         feedback();
          deze regel mag je weg halen voor de bonus opdracht */
 
-    }
-
     /*
      Deze methode is voor de bonus opdracht.
      */
-    public static void feedback(String stringnumber) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder feedback = new StringBuilder();
-        System.out.println("take a guess");
-        String guess = scanner.nextLine();
-        if (Objects.equals(guess, stringnumber)) {
-            System.out.println("gefeliciteerd je hebt het goed");
-        } else {
-            for (int i = 0; i < 4; i++) {
-                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
-                    feedback.append("+");
-                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
-                    feedback.append("0");
-                } else {
-                    feedback.append("X");
-                }
-            }
-        }
-        System.out.println(feedback.toString());
-    }
-}
+//    public static void feedback(String stringnumber) {
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder feedback = new StringBuilder();
+//        System.out.println("take a guess");
+//        String guess = scanner.nextLine();
+//        if (Objects.equals(guess, stringnumber)) {
+//            System.out.println("gefeliciteerd je hebt het goed");
+//        } else {
+//            for (int i = 0; i < 4; i++) {
+//                if (guess.substring(i, i + 1).equals(stringnumber.substring(i, i + 1))) {
+//                    feedback.append("+");
+//                } else if (stringnumber.contains(guess.substring(i, i + 1))) {
+//                    feedback.append("0");
+//                } else {
+//                    feedback.append("X");
+//                }
+//            }
+//        }
+//        System.out.println(feedback.toString());
+//    }
+//}
---
+++ src/Translator.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+public class Translator {
+
+    //HashMap Declaration
+    HashMap<String[], Integer[]> numericAlpha = new HashMap<String[], Integer[]>();
+
+    //Constructor
+    String String;
+    int[] Integer;
+
+    public Translator (String alphabetic, Integer numeric) {
+        this.String = new String[alphabetic];
+        this.Integer = new Integer[]{numeric};
+    }
+
+        for (int i = 0; i < al.size(); i++) {
+        System.out.println(al.get(i));
+    }
+
+}
+
+
+
